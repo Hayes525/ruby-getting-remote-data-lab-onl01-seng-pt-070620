f3d14c515357e5cc1e17ee284c4da43f8b9d9241
Jordan Hayes <nosafoundation@gmail.com>
2020-08-05 22:49:11 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -12,8 +12,6 @@ class GetRequester
         :location => project.css("ul.project-meta span.location-name").text,
         :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
       }
-  def get_response_body
-
-  end
+  
 
 end
