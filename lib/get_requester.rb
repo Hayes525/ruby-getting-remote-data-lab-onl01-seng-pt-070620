6459766267e66ff34c0213fe7ce623d13250aa2e
Jordan Hayes <nosafoundation@gmail.com>
2020-08-05 22:49:19 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,7 +2,7 @@ class GetRequester
   html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
 
-    projects = {}
+    
 
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
