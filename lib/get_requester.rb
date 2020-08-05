e2d74e853c02a82b4cd3912222379224c0ee8142
Jordan Hayes <nosafoundation@gmail.com>
2020-08-05 22:49:47 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,15 +3,17 @@ class GetRequester
     kickstarter = Nokogiri::HTML(html)
 
     
-
-    kickstarter.css("li.project.grid_4").each do |project|
-      title = project.css("h2.bbcard_name strong a").text
-      projects[title.to_sym] = {
-        :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-        :description => project.css("p.bbcard_blurb").text,
-        :location => project.css("ul.project-meta span.location-name").text,
-        :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-      }
-
+def method_name
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  
+end
+    
 
 end
